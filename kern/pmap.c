7e6d0d5353198636af3e4ff94197e692d46b63b7
Alex Khalifer <shura100@campus.technion.ac.il>
2015-04-16 14:01:52 +0300

    Done Ex1

--- kern/pmap.c
+++ kern/pmap.c
@@ -196,7 +196,12 @@ mem_init(void)
 	//    - the new image at UENVS  -- kernel R, user R
 	//    - envs itself -- kernel RW, user NONE
 	// LAB 3: Your code here.
-
+	boot_map_region(
+			kern_pgdir,
+			UENVS,
+			ROUNDUP(sizeof(struct Env) * NENV, PGSIZE),
+			PADDR(envs),
+			PTE_U | PTE_P);
 	//////////////////////////////////////////////////////////////////////
 	// Use the physical memory that 'bootstack' refers to as the kernel
 	// stack.  The kernel stack grows down from virtual address KSTACKTOP.
