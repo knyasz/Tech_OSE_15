fd639d142d01457c8fe32202e475fa040319baee
Alex Khalifer <shura100@campus.technion.ac.il>
2015-06-10 21:55:36 +0300

    Ex3 Ex4 done

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,3 +1,10 @@
 #include <kern/e1000.h>
+#include <kern/pmap.h>
 
 // LAB 6: Your driver code here
+int e1000_pci_attach(struct pci_func *pcif){
+	pci_func_enable(pcif);
+	e1000_bar0 = mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
+	cprintf("Should be 0x80080783 : 0x%08x \n",e1000_bar0[BYTE_TO_WORD(E1000_STATUS)]);
+	return 0;
+}
--- kern/e1000.h
+++ kern/e1000.h
@@ -1,4 +1,19 @@
 #ifndef JOS_KERN_E1000_H
 #define JOS_KERN_E1000_H
 
+#include <kern/pci.h>
+
+
+volatile  uint32_t * e1000_bar0;
+
+#define BYTE_TO_WORD(byte) ( (byte) >> 2 )
+
+#define E1000_VENDOR_ID 			0x8086
+#define E1000_DEV_ID_82540EM		0x100E
+
+#define E1000_STATUS				0x00008  /* Device Status - RO */
+
+
+int e1000_pci_attach(struct pci_func *pcif);
+
 #endif	// JOS_KERN_E1000_H
