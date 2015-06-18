bd40bada308ad7b631a7e2ad3b94279f0355df29
Alex Khalifer <shura100@campus.technion.ac.il>
2015-06-18 04:01:10 +0300

    Ex5 Tx init: user panic in ns at lib/ipc.c:67: ipc_send : sys_ipc_try_send error : bad environment.
    E1000_DEBUG=TXERR,TX qemu to run your test. 
    You should see something like
    
    e1000: index 0: 0x271f00 : 9000002a 0
    
    nothing like that !!!
    qemu -hda obj/kern/kernel.img 
    	 -serial mon:stdio 
    	 -gdb tcp::26000 -D qemu.log 
    	 -smp 1 -hdb obj/fs/fs.img 
    	 -net user 
    	 -net nic,model=e1000 
    	 -redir tcp:26001::7 
    	 -redir tcp:26002::80 
    	 -redir udp:26001::7 
    	 -net dump,file=qemu.pcap 
    PCI: 00:00.0: 8086:1237: class: 6.0 (Bridge device) irq: 0
    PCI: 00:01.0: 8086:7000: class: 6.1 (Bridge device) irq: 0
    PCI: 00:01.1: 8086:7010: class: 1.1 (Storage controller) irq: 0
    PCI: 00:01.3: 8086:7113: class: 6.80 (Bridge device) irq: 9
    PCI: 00:02.0: 1013:00b8: class: 3.0 (Display controller) irq: 0
    PCI: 00:03.0: 8086:100e: class: 2.0 (Network controller) irq: 11
    PCI function 00:03.0 (8086:100e) enabled
    Should be 0x80080783 : 0x80080783

--- kern/e1000.c
+++ kern/e1000.c
@@ -1,10 +1,128 @@
 #include <kern/e1000.h>
 #include <kern/pmap.h>
 
+#include <inc/stdio.h>
+#include <inc/assert.h>
+#include <inc/string.h>
+
 // LAB 6: Your driver code here
+
+// Transmit descriptors
+tx_desctiptor tx_descriptors[E1000_NUM_OF_TX_DESCRIPTORS] __attribute__ ((aligned (16)));
+// Transmit buffers
+tx_packet_buffer tx_packet_buffers[E1000_NUM_OF_TX_DESCRIPTORS];
+
 int e1000_pci_attach(struct pci_func *pcif){
+
+	//Enable PCI device - Ex3
 	pci_func_enable(pcif);
-	e1000_bar0 = mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
-	cprintf("Should be 0x80080783 : 0x%08x \n",e1000_bar0[BYTE_TO_WORD(E1000_STATUS)]);
+
+	//Memory map I/O for PCI device - Ex4
+	p_e1000_MMIO = mmio_map_region(pcif->reg_base[0],pcif->reg_size[0]);
+	cprintf("Should be 0x80080783 : 0x%08x \n", p_e1000_MMIO[E1000_STATUS]);
+	assert(p_e1000_MMIO[E1000_STATUS] == 0x80080783);
+
+	//Transmit Initialization
+	e1000_tx_init();
+
+
 	return 0;
+}
+
+
+void e1000_tx_init(){
+	/*****************************************************
+	 Transmit Initialization 14.5
+	 *****************************************************/
+	/*
+	 * Allocate a region of memory for the transmit descriptor list.
+	 * Software should insure this memory is aligned on a paragraph (16-byte)
+	 * boundary. - static array tx_descriptors[].
+	 * Initialize tx_descriptors[]
+	 */
+	//Initialize TX descriptors - Ex5
+	memset(	tx_descriptors,
+			0,
+			sizeof(tx_desctiptor) * E1000_NUM_OF_TX_DESCRIPTORS);
+
+	//Initialize TX buffers - Ex5
+	memset(	tx_packet_buffers,
+			0,
+			sizeof(tx_packet_buffer) * E1000_NUM_OF_TX_DESCRIPTORS);
+
+	//Connect descriptors to buffers - Ex5
+	int i;
+	for (i = 0; i < E1000_NUM_OF_TX_DESCRIPTORS; i++) {
+		tx_descriptors[i].addr = PADDR(tx_packet_buffers[i].buffer);
+		/*
+		 * Need feedback from HW that the packet is sent:
+		 * If you set the RS bit in the command field of a transmit descriptor,
+		 * then, when the card has transmitted the packet in that descriptor,
+		 * the card will set the DD bit in the status field of the descriptor.
+		 */
+		tx_descriptors[i].cmd |= E1000_TXD_CMD_RS;
+		/*
+		 * If a descriptor's DD bit is set,
+		 * you know it's safe to recycle that descriptor
+		 * and use it to transmit another packet.
+		 */
+		tx_descriptors[i].status |= E1000_TXD_STAT_DD;
+	}
+
+	/*
+	 * Program the Transmit Descriptor Base Address (TDBAL/TDBAH) register(s)
+	 * with the address of the region.
+	 * TDBAL is used for 32-bit addresses and both TDBAL and TDBAH
+	 * are used for 64-bit addresses.
+	 */
+	p_e1000_MMIO[E1000_TDBAL] = PADDR(tx_descriptors);
+	p_e1000_MMIO[E1000_TDBAH] = 0;
+
+	/*
+	 * Set the Transmit Descriptor Length (TDLEN) register
+	 * to the size (in bytes) of the descriptor ring.
+	 * This register must be 128-byte aligned.
+	 */
+	p_e1000_MMIO[E1000_TDLEN] = sizeof(tx_desctiptor) *
+									E1000_NUM_OF_TX_DESCRIPTORS;
+
+	/*
+	 * The Transmit Descriptor Head and Tail (TDH/TDT)
+	 * registers are initialized (by hardware) to 0b
+	 * after a power-on or a software initiated Ethernet controller reset.
+	 * Software should write 0b to both these registers to ensure this.
+	 */
+	p_e1000_MMIO[E1000_TDH] = 0;
+	p_e1000_MMIO[E1000_TDT] = 0;
+
+	/* Initialize the Transmit Control Register (TCTL) for desired operation
+	 * to include the following:
+	 */
+
+	 // Set the Enable (TCTL.EN) bit to 1b for normal operation.
+	p_e1000_MMIO[E1000_TCTL] |= E1000_TCTL_EN;
+
+	// Set the Pad Short Packets (TCTL.PSP) bit to 1b.
+	p_e1000_MMIO[E1000_TCTL] |= E1000_TCTL_PSP;
+
+	// Configure the Collision Threshold (TCTL.CT) to the desired value.
+	// Ethernet standard is 10h.
+	// This setting only has meaning in half duplex mode
+	p_e1000_MMIO[E1000_TCTL] &= ~E1000_TCTL_CT;
+	p_e1000_MMIO[E1000_TCTL] |= E1000_TCTL_CT_VALUE;
+
+	// Configure the Collision Distance (TCTL.COLD) to its expected value.
+	// For full duplex	operation, this value should be set to 40h.
+	// For gigabit half duplex, this value should be set to	200h.
+	// For 10/100 half duplex, this value should be set to 40h.
+	p_e1000_MMIO[E1000_TCTL] &= ~E1000_TCTL_COLD;
+	p_e1000_MMIO[E1000_TCTL] |= E1000_TCTL_COLD_VALUE;
+
+	// Program the Transmit IPG (TIPG) register
+	// with the  decimal values from Table 13-77. TIPG Register Bit Description.
+	// to get the minimum legal Inter Packet Gap
+	p_e1000_MMIO[E1000_TIPG] = 0;
+	p_e1000_MMIO[E1000_TIPG] |= E1000_TIPG_IPGT_VALUE;
+	p_e1000_MMIO[E1000_TIPG] |= E1000_TIPG_IPGR1_VALUE;
+	p_e1000_MMIO[E1000_TIPG] |= E1000_TIPG_IPGR2_VALUE;
 }
--- kern/e1000.h
+++ kern/e1000.h
@@ -4,16 +4,90 @@
 #include <kern/pci.h>
 
 
-volatile  uint32_t * e1000_bar0;
+volatile  uint32_t * p_e1000_MMIO;
 
+/*
+ * Our p_e1000_MMIO array has each cell of 32 bits,
+ * but e1000 registers use the offset in bytes,
+ * thus to use our array we need to translate bytes indexes
+ * to 32-bit words indexes of the array.
+ */
 #define BYTE_TO_WORD(byte) ( (byte) >> 2 )
 
 #define E1000_VENDOR_ID 			0x8086
 #define E1000_DEV_ID_82540EM		0x100E
 
-#define E1000_STATUS				0x00008  /* Device Status - RO */
+
+// We will support the maximum number of descriptors allowed for lab 6 - 64
+#define E1000_NUM_OF_TX_DESCRIPTORS 		64
+#define TX_PACKET_SIZE				1518 // Bytes - uint_8t
+
+/* Device Status - RO */
+#define E1000_STATUS				BYTE_TO_WORD(0x00008)
+/* TX Descriptor Base Address Low - RW */
+#define E1000_TDBAL					BYTE_TO_WORD(0x03800)
+/* TX Descriptor Base Address High - RW */
+#define E1000_TDBAH					BYTE_TO_WORD(0x03804)
+/* TX Descriptor Length - RW */
+#define E1000_TDLEN					BYTE_TO_WORD(0x03808)
+/* TX Descriptor Head - RW */
+#define E1000_TDH					BYTE_TO_WORD(0x03810)
+/* TX Descriptor Tail - RW */
+#define E1000_TDT					BYTE_TO_WORD(0x03818)
+/* TX Control - RW */
+#define E1000_TCTL					BYTE_TO_WORD(0x00400)
+
+/* Transmit Control */
+#define E1000_TCTL_EN				0x00000002	/* enable tx */
+#define E1000_TCTL_PSP				0x00000008	/* pad short packets */
+#define E1000_TCTL_CT				0x00000ff0	/* collision threshold */
+#define E1000_TCTL_CT_VALUE			0x00000100	/* collision threshold value */
+#define E1000_TCTL_COLD				0x003ff000	/* collision distance */
+//For full duplex	operation, this value should be set to 40h.
+#define E1000_TCTL_COLD_VALUE		0x00040000	/* collision distance value*/
+
+/* TX Inter-packet gap -RW */
+#define E1000_TIPG					BYTE_TO_WORD(0x00410)
+// The value that should be programmed into IPGT is 10
+#define E1000_TIPG_IPGT_VALUE		10
+// The value that should be programmed into IPGR1 is eight.
+#define E1000_TIPG_IPGR1_VALUE		8 << 10
+// the value that should be programmed into IPGR2 is six
+#define E1000_TIPG_IPGR2_VALUE		6 << 20
+
+/* Descriptor Status */
+#define E1000_TXD_STAT_DD    0x00000001 /* Descriptor Done */
+#define E1000_TXD_CMD_RS     0x08000000 /* Report Status */
+
+
+
+/*
+ * Legacy transmit descriptor - Table 3-9
+ */
+struct tx_desc
+{
+	uint64_t addr;
+	uint16_t length;
+	uint8_t cso;
+	uint8_t cmd;
+	uint8_t status;
+	uint8_t css;
+	uint16_t special;
+} __attribute__((packed));// minimum required memory will be used
+typedef struct tx_desc tx_desctiptor;
+
+/*
+ * The maximum size of an Ethernet packet is 1518 bytes,
+ * which bounds how big the buffer needs to be.
+ */
+struct tx_packet_buffer
+{
+	uint8_t buffer[TX_PACKET_SIZE];
+} __attribute__((packed));// minimum required memory will be used
+typedef struct tx_packet_buffer tx_packet_buffer;
 
 
 int e1000_pci_attach(struct pci_func *pcif);
+void e1000_tx_init();
 
 #endif	// JOS_KERN_E1000_H
